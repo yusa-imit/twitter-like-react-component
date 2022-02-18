2b06a24a8624030b2f90fefc7539fdeee16e3e49
yusa-imit <esimileiar81@gmail.com>
2022-02-18 19:23:28 +0900

    delete node 10.x test from github ci/cd

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./stories/*"],
   content: [],
   theme: {
+    screens: {
+        "sm": "500px",
+        "md": "1005px",
+        "lg": "1095px",
+        "xl": "1280px"
+    },
     extend: {},
   },
   plugins: [],
