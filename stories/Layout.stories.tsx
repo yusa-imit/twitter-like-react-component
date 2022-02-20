d195f302c998a32e15ed53515ffafd4512c325ae
yusa-imit <esimileiar81@gmail.com>
2022-02-20 18:06:06 +0900

    prettier setting

--- stories/Layout.stories.tsx
+++ stories/Layout.stories.tsx
@@ -1,21 +1,18 @@
 import { Meta, Story } from '@storybook/react';
 import React from 'react';
-import Layout, {LayoutProps} from '../src/components/Layout';
+import Layout, { LayoutProps } from '../src/components/Layout';
 
 const meta: Meta = {
-    title: "Layout",
+    title: 'Layout',
     component: Layout,
     argTypes: {
-        mainStyle: {
-            
-        }
+        mainStyle: {},
     },
-
-}
+};
 
 export default meta;
 
-const Template: Story<LayoutProps> = args => Layout(args);
+const Template: Story<LayoutProps> = (args) => Layout(args);
 
 export const Default = Template.bind({});
 
