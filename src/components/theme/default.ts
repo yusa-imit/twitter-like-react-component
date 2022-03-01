a3fd926dc06162a4f06501e9451f39fec04f3f72
yusa-imit <esimileiar81@gmail.com>
2022-03-01 18:24:58 +0900

    theme

--- src/components/theme/default.ts
+++ src/components/theme/default.ts
@@ -1,3 +1,26 @@
-export const basic = {
-    primary: "#"
-}
+export const backgroundColors = {
+    light: 'rgb(255, 255, 255)',
+    dark: 'rgb(21, 32, 43)',
+    darker: 'rgb(0, 0, 0)',
+};
+
+export const textColors = {
+    light: 'rgb(0, 0, 0)',
+    dark: 'rgb(255, 255, 255)',
+    darker: 'rgb(255, 255, 255)',
+};
+
+export const subTextColors = {
+    light: 'rgb(83, 100, 113)',
+    dark: 'rgb(136, 153, 166)',
+    darker: 'rgb(110, 118, 125)',
+};
+
+export const primaryColors = {
+    blue: 'rgb(29, 155, 240)',
+    yellow: 'rgb(255, 212, 0)',
+    pink: 'rgb(249, 24, 128)',
+    purple: 'rgb(120, 86, 255)',
+    orange: 'rgb(255, 122, 0)',
+    green: 'rgb(0, 186, 124)',
+};
